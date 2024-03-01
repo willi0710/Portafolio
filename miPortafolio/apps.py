93767d0c4943441ec5575ca2d531d4094398c800
willi0710 <106782429+willi0710@users.noreply.github.com>
2024-03-01 23:25:34 +0000

    w

--- miPortafolio/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MiportafolioConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'miPortafolio'
--- proyectos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import project_render
-
-
-app_name = 'proyectos'
-urlpatterns =[
-    path('', project_render, name= 'project' )
-]
